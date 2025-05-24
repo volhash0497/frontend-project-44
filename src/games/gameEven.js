c4d82fce97972a5bfa1ea545d3a61c10ddf188f4
Volha <v.shyshkina97@gmail.com>
2025-05-24 16:54:55 +0200

    refactor

--- src/games/gameEven.js
+++ src/games/gameEven.js
@@ -1,13 +1,13 @@
 import engine from '../index.js'
 import randomNumFromInterval from '../getRandomNumber.js'
 
-const isNumberEven = num => num % 2 === 0 ? 'yes' : 'no'
+const isNumberEven = num => num % 2 === 0 ? true : false
 
 const question = ('Answer "yes" if the number is even, otherwise answer "no".')
 
 const getData = () => {
   const expression = randomNumFromInterval(0, 100)
-  const correctAnswer = isNumberEven(expression)
+  const correctAnswer = isNumberEven(expression) ? 'yes' : 'no'
 
   return [expression, correctAnswer]
 }
